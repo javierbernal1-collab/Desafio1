72d70939c12cc9c71e2472c20087bb4b8fe42b1c
javierbernal1-collab <javier.bernal1@udea.edu.co>
2025-09-27 08:19:30 -0500

    segundo comit - funcion de desencriptado general

--- Desafio1/main.cpp
+++ Desafio1/main.cpp
@@ -16,6 +16,15 @@ unsigned char aplicarXOR(unsigned char valor, unsigned char clave) {
     return valor ^ clave;
 }
 
+// Función para desencriptar un arreglo
+void desencriptar(unsigned char *entrada, unsigned char *salida, int longitud, int n, unsigned char clave) {
+    for (int i = 0; i < longitud; i++) {
+        unsigned char temp = aplicarXOR(entrada[i], clave);
+        temp = rotarDerecha(temp, n);
+        salida[i] = temp;
+    }
+}
+
 int main() {
     unsigned char mensaje[5] = {65, 66, 67, 68, 69}; // Simulación (A B C D E)
     unsigned char salida[5];
@@ -23,12 +32,8 @@ int main() {
     int n = 3;              // rotaciones
     unsigned char clave = 0x5A;  // clave XOR
 
-    // Desencriptar manualmente (ejemplo pequeño)
-    for (int i = 0; i < 5; i++) {
-        unsigned char temp = aplicarXOR(mensaje[i], clave);
-        temp = rotarDerecha(temp, n);
-        salida[i] = temp;
-    }
+    desencriptar(mensaje, salida, 5, n, clave);
+
 
     cout << "Mensaje desencriptado: ";
     for (int i = 0; i < 5; i++) {
